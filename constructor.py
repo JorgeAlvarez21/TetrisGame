7fdd9fcc9962c3e361440712672006771242c866
JorgeAlvarez21 <joglnvz@gmail.com>
2022-04-16 02:45:46 -0500

    Re-doing main game logic, changing active shape grid to coordinates

--- constructor.py
+++ constructor.py
@@ -7,11 +7,8 @@ class Builder():
         #Dimensions
         self.disp_width = 700
         self.disp_height = 850
-        self.objSize = width, height = (30, 40)
         self.blockSize = width, height = (34, 35)
         self.frameSize = width, height = (340, 700)
-        self.saved_settled_blocks = {}
-        self.settled_blocks = {}
 
         #Grid
         self.wideGrid = [170, 204, 238, 272, 306, 340, 374, 408, 442, 476, 510]
@@ -23,19 +20,10 @@ class Builder():
                        7: 345, 8: 380, 9: 415, 10: 450, 11: 485, 12: 520, 13: 555,
                        14: 590, 15: 625, 16: 660, 17: 695, 18: 730, 19: 765, 20: 800}
 
-        #Levels
-        self.level1time = 2
+        self.invxgrid = {170: 0, 204: 1, 238: 2, 272: 3, 306: 4, 340: 5, 374: 6, 408: 7, 442: 8, 476: 9, 510: 10}
 
-        #Grid Axis
-        self.xaxisB = [x for x in range(11)]
-        self.yaxisB = [y for y in range(21)]
-
-        #Grid Blocks
-        self.grid_blocks = {}
-        for y in range(-3, 21):
-            for x in range(-3, 11):
-                self.grid_blocks[x, y] = 0
-
+        self.invygrid = {100: 0, 135: 1, 170: 2, 205: 3, 240: 4, 275: 5, 310: 6, 345: 7, 380: 8, 415: 9, 450: 10,
+                         485: 11, 520: 12, 555: 13, 590: 14, 625: 15, 660: 16, 695: 17, 730: 18, 765: 19, 800: 20}
         #Colors
         self.grey = (61,61,66)
         self.light_grey = (20,20,60)
@@ -44,71 +32,80 @@ class Builder():
         self.blue = (102,102,255)
         self.gridc = (45,45,45)
 
-        #Font
-        # self.smallfont = pygame.font.SysFont('Corbel',35)
+        self.block_count = 0
 
         #Motion
-        self.velocity = .8
-        # self.x = 200
-        # self.y = 180
         self.pressed_keys = {"left": False, "right": False, "up": False, "down": False}
 
         #Timer and FPS
-        self.FPS = 60
-        self.FramePerSec = pygame.time.Clock()
-        self.whiteblocks = {'row0' : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row1': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row2': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row3': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row4': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row5': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row6': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row7': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row8': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row9': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row10': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row11': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row12': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row13': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row14': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row15': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row16': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row17': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row18': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row19': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            'row20': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+        self.main_shape = {   '0': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '1': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '2': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '3': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '4': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '5': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '6': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '7': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '8': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '9': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '10':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '11': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '12': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '13': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '14': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '15': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '16': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '17': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '18': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '19': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              '20': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                              '21': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}
+
+        self.floor_bounds = {'0': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                             '1': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                             '2': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                             '3': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                             '4': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                             '5': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                             '6': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                             '7': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                             '8': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                             '9': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                            '10': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1] }
+
+        self.settled_blocks = {}
+
+
 
     def Display(self):
         self.display = pygame.display.set_mode((self.disp_width, self.disp_height))
         pygame.display.set_caption("Tetris Game")
         return self.display
 
+    def gen_FigID(self):
+        self.block_count += 1
+        code = str(self.block_count)+'block'
+        return code
 
-    def fig_toSave(self, xls, yls):
-        import string
-        import random
-        def gen_code():
-            letters = string.ascii_lowercase
-            letters = [x for x in letters]
-            numbers = [str(n) for n in range(9)]
-            lets = random.choices(letters, k=3)
-            nums = random.choices(numbers, k=2)
-            code = [lets[0]] + [nums[0]] + [lets[1]] + [nums[1]] + [lets[2]]
-            code = ''.join(code)
-            return code
-
-        for x, y in zip(xls, yls):
-            code = gen_code()
-            figId = f'figure{code}'
-            self.saved_settled_blocks[figId] = pygame.Surface(self.blockSize)
-            self.settled_blocks[figId] = [x, y]
 
     def blocks_meta(self, xpos, ypos):
-        for block, value in self.grid_blocks.items():
-            for x, y in zip(xpos, ypos):
-                if block == (x, y):
-                    self.grid_blocks[(x, y)] = 1
+        #Saving corresponding coords and ID
+        for x, y in zip(xpos, ypos):
+            blockID = str(x) + 'x' + str(y) + 'y'
+            blockID = pygame.Surface(self.blockSize)
+            self.settled_blocks[blockID] = [self.xgridB[x], self.ygridB[y]]
 
+        #Update main_shape
+        for k in self.main_shape.keys():
+            for x, y in zip(xpos, ypos):
+                if k == str(y):
+                    self.main_shape[k][x] = 1
+
+
+        pass
+
+    def debug(self, *args):
+        print(args)
 
 
 
